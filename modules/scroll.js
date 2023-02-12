419ce7d3da87a3aa23b3b0c38e393241c7ac299e
Seohyun Jung <iseohyun@hanmail.net>
2023-02-12 17:40:57 +0900

    scroll bug fix

--- modules/scroll.js
+++ modules/scroll.js
@@ -5,7 +5,7 @@ $(window).scroll(function () {
 
   if (scrollTop >= 100) { // 숫자가 클수록 스크롤을 많이 내려야 메뉴가 사라집니다.
     if ((scrollTop > lastScrollTop) && (lastScrollTop > 0)) {
-      $("header").css("top", "-59px");    // 얼마나 메뉴를 위로 끌어 올릴지 결정합니다.
+      $("header").css("top", "-" + $("header").height() + "px");    // 얼마나 메뉴를 위로 끌어 올릴지 결정합니다.
     } else {
       $("header").css("top", "0px");
     }
